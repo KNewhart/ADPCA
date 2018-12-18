512c6b820f1bfb40c376c7b300b67bed641e6bc9
KNewhart <kbnewhart@mines.edu>
2018-12-18 15:52:31 -0700

    Add folder option

--- R/loadDBF.R
+++ R/loadDBF.R
@@ -4,6 +4,7 @@
 #' @param dataLocation String excluding ending backslashes of LogAllData folder
 #' @param endDay Date object with the last date to be included
 #' @param nDays Number of days of data to compile
+#' @param folder LogAllData or Log60sec
 #' @return compiledData, Dataframe with all original data from .DBF files.
 #' @examples
 #' rawData <- loadDBF(dataLocation, testingDay, nDays)
@@ -14,7 +15,7 @@
 # (where "LogAllData" is located including fial backslashes),
 # the last day to be compiled, and the number of previous days to include
 
-loadDBF <- function(dataLocation, endDay, nDays) {
+loadDBF <- function(dataLocation, endDay, nDays, folder = "LogAllData") {
   # Check if package is included already, if not then load
   library("foreign")
 
@@ -36,7 +37,7 @@ loadDBF <- function(dataLocation, endDay, nDays) {
         dateString <- paste(dates$currentYear,sprintf("%02d",dates$currentMonth),sprintf("%02d",dates$currentDay),sep=' ')
 
         ### Check to see if file exists:
-        if (!file.exists(paste(dataLocation,"LogAllData\\",dateString," 0000 LogAllData (Wide).DBF",sep=''))) {
+        if (!file.exists(paste(dataLocation,folder,"\\",dateString," 0000 ",folder," (Wide).DBF",sep=''))) {
           # If the file does not exist, and the date is the 31st...
           if (dates$currentDay > 31) {
             # ... the end of the month has been reached, so break from the daily 'while' loop
@@ -60,7 +61,7 @@ loadDBF <- function(dataLocation, endDay, nDays) {
         # Create an empty dataframe for the raw, imported data
         rawData <- NULL
         # Attempt to open file. If file can be opened, write to dataframe.
-        tryCatch(rawData <- read.dbf(paste(dataLocation,"\\LogAllData\\",dateString," 0000 LogAllData (Wide).DBF",sep='')),error=function(e){})
+        tryCatch(rawData <- read.dbf(paste(dataLocation,folder, "\\",dateString," 0000 ",folder," (Wide).DBF",sep='')),error=function(e){})
         # If the file could not be opened, the dataframe is still empty.
         if (length(rawData) == 0) {
           # Since file could not be opened, move onto the next day
--- R/loadandcleanDBF.R
+++ R/loadandcleanDBF.R
@@ -4,6 +4,7 @@
 #' @param dataLocation String excluding ending backslashes of LogAllData folder
 #' @param testingDay Date object with the last date to be included
 #' @param nDays Number of days of data to compile
+#' @param folder LogAllData or Log60sec
 #' @return rawData, Dataframe including all compiled data
 #' @examples
 #' dataLocation <- "MP SB-MBR Data\\"
@@ -12,13 +13,13 @@
 #' rawData <- loadandcleanDBF(dataLocation, testingDay, nDays)
 #' @export
 
-loadandcleanDBF <- function (dataLocation, testingDay, nDays) {
+loadandcleanDBF <- function (dataLocation, testingDay, nDays, folder = "LogAllData") {
 
   # Compile raw data from data files (no column names, extraneous empty columns)
-  rawData <- loadDBF(dataLocation, testingDay, nDays)
+  rawData <- loadDBF(dataLocation, testingDay, nDays, folder)
 
   # Read in data labels from 'Tagname' file
-  columnNames <- read.dbf(paste(dataLocation,"LogAllData\\",format(testingDay, format = "%Y %m %d")," 0000 LogAllData (Tagname).DBF",sep=''))
+  columnNames <- read.dbf(paste(dataLocation,folder, "\\",format(testingDay, format = "%Y %m %d")," 0000 ", folder, " (Tagname).DBF",sep=''))
 
   # Run Cleaner function (rename columns, remove extraneous columns)
   rawData <- cleanDBF(rawData, columnNames)
