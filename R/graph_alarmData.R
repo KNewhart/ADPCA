d014eb06fc9b373207dc1db901cc646ada378cb3
KNewhart <kbnewhart@mines.edu>
2019-01-03 13:41:03 -0700

    something

--- R/graph_alarmData.R
+++ R/graph_alarmData.R
@@ -22,113 +22,116 @@ graph_alarmData <- function (data, keyword="", trueFaultTime = NULL) {
     } else {
       filename <- paste(keyword, "allGraphs",as.Date(r[1]), as.Date(r[2]))
     }
+
+  plot.it <- function() {
+    par(mfrow=c((n),1), mar = c(2.1, 4.1, 4.1, 2.1))
+
+    for (i in 1:n) {
+
+      if (i %in% tmp) {
+        plot(x = dates,
+             y = data[,i],
+             xaxt="n",
+             ylim=c(0,7),
+             xlab = "",
+             ylab = "",
+             main = colnames(data)[i],
+             type = "p",
+             pch = 16,
+             #col = "black")
+             col = ifelse(alarms == 0, "black", ifelse (alarms == 1, "red", ifelse (alarms == 2, "orange", "purple"))))
+        if (r.hours > 24) {
+          axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+        } else {
+          axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
+        }
+
+        if (!is.null(trueFaultTime)) {
+          abline(v=trueFaultTime, col="blue")
+        } else {}
+
+        legend("bottomleft",
+               legend = c("IC", "T2", "SPE", "Both"),
+               col = c("black", "red", "orange", "purple"),
+               pch = 16,
+               bty = "n")
+
+      } else {
+        if (i %in% do) {
+          plot(x = dates,
+               y = data[,i],
+               xaxt="n",
+               ylim=c(0,3),
+               xlab = "",
+               ylab = "",
+               main = colnames(data)[i],
+               type = "p",
+               pch = 16,
+               #col = "black")
+               col = ifelse(alarms == 0, "black", ifelse (alarms == 1, "red", ifelse (alarms == 2, "orange", "purple"))))
+
+          # line.ewma <- ewma(data[,i], n = 1440, alpha = 0.01)
+          # points(x = index(line.ewma),
+          #        y = line.ewma, col = "red")
+
+          if (r.hours > 24) {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+          } else {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
+          }
+
+          if (!is.null(trueFaultTime)) {
+            abline(v=trueFaultTime, col="blue")
+          } else {}
+
+          legend("bottomleft",
+                 legend = c("IC", "T2", "SPE", "Both"),
+                 col = c("black", "red", "orange", "purple"),
+                 pch = 16,
+                 bty = "n")
+
+        } else {
+          plot(x = dates,
+               y = data[,i],
+               xaxt="n",
+               xlab = "",
+               ylab = "",
+               main = colnames(data)[i],
+               type = "p",
+               pch = 16,
+               #col = "black")
+               col = ifelse(alarms == 0, "black", ifelse (alarms == 1, "red", ifelse (alarms == 2, "orange", "purple"))))
+
+          # line.ewma <- ewma(data[,i])
+          # line(x = index(line.ewma),
+          #      y = line.ewma)
+          if (r.hours > 24) {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+          } else {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
+          }
+          # axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+          if (!is.null(trueFaultTime)) {
+            abline(v=trueFaultTime, col="blue")
+
+          } else {}
+
+          legend("bottomleft",
+                 legend = c("IC", "T2", "SPE", "Both"),
+                 col = c("black", "red", "orange", "purple"),
+                 pch = 16,
+                 bty = "n")
+        }
+      }
+    }
+
+  }
   # png(file = paste(filename, ".png", sep=""), units = "in", res = 96, width = 11, height =n*3)
   # pdf(file = paste(filename,".pdf", sep=''),
   #     width = 30,
   #     height = (n)*2)
   svg(filename = paste(filename, ".svg", sep=""), width = 8, height =n*2, family = "serif")
-
-  par(mfrow=c((n),1), mar = c(2.1, 4.1, 4.1, 2.1))
-
-  for (i in 1:n) {
-
-    if (i %in% tmp) {
-      plot(x = dates,
-           y = data[,i],
-           xaxt="n",
-           ylim=c(0,7),
-           xlab = "",
-           ylab = "",
-           main = colnames(data)[i],
-           type = "p",
-           pch = 16,
-           #col = "black")
-           col = ifelse(alarms == 0, "black", ifelse (alarms == 1, "red", ifelse (alarms == 2, "orange", "purple"))))
-      if (r.hours > 24) {
-        axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-      } else {
-        axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
-      }
-
-      if (!is.null(trueFaultTime)) {
-        abline(v=trueFaultTime, col="blue")
-      } else {}
-
-      legend("bottomleft",
-             legend = c("IC", "T2", "SPE", "Both"),
-             col = c("black", "red", "orange", "purple"),
-             pch = 16,
-             bty = "n")
-
-      } else {
-      if (i %in% do) {
-        plot(x = dates,
-             y = data[,i],
-             xaxt="n",
-             ylim=c(0,3),
-             xlab = "",
-             ylab = "",
-             main = colnames(data)[i],
-             type = "p",
-             pch = 16,
-             #col = "black")
-             col = ifelse(alarms == 0, "black", ifelse (alarms == 1, "red", ifelse (alarms == 2, "orange", "purple"))))
-
-        # line.ewma <- ewma(data[,i], n = 1440, alpha = 0.01)
-        # points(x = index(line.ewma),
-        #        y = line.ewma, col = "red")
-
-        if (r.hours > 24) {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-        } else {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
-        }
-
-        if (!is.null(trueFaultTime)) {
-          abline(v=trueFaultTime, col="blue")
-        } else {}
-
-        legend("bottomleft",
-               legend = c("IC", "T2", "SPE", "Both"),
-               col = c("black", "red", "orange", "purple"),
-               pch = 16,
-               bty = "n")
-
-      } else {
-        plot(x = dates,
-             y = data[,i],
-             xaxt="n",
-             xlab = "",
-             ylab = "",
-             main = colnames(data)[i],
-             type = "p",
-             pch = 16,
-             #col = "black")
-             col = ifelse(alarms == 0, "black", ifelse (alarms == 1, "red", ifelse (alarms == 2, "orange", "purple"))))
-
-        # line.ewma <- ewma(data[,i])
-        # line(x = index(line.ewma),
-        #      y = line.ewma)
-        if (r.hours > 24) {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-        } else {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
-        }
-        # axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-        if (!is.null(trueFaultTime)) {
-          abline(v=trueFaultTime, col="blue")
-
-        } else {}
-
-        legend("bottomleft",
-               legend = c("IC", "T2", "SPE", "Both"),
-               col = c("black", "red", "orange", "purple"),
-               pch = 16,
-               bty = "n")
-      }
-    }
-  }
-
+  plot.it()
   dev.off()
-
+  # plot.it() # Margins too large to plot
 }
--- R/graph_rawData.R
+++ R/graph_rawData.R
@@ -21,80 +21,85 @@ graph_rawData <- function (data, keyword="", trueFaultTime = NULL) {
     } else {
       filename <- paste(keyword, "allGraphs",as.Date(r[1]), as.Date(r[2]))
     }
+
+
+
+  plot.it <- function() {
+    par(mfrow=c((n),1), mar = c(2.1, 4.1, 4.1, 2.1))
+
+    for (i in 1:n) {
+
+      if (i %in% tmp) {
+        plot(x = dates,
+             y = data[,i],
+             xaxt="n",
+             ylim=c(0,7),
+             xlab = "",
+             ylab = "",
+             main = colnames(data)[i],
+             type = "p",
+             col = "black")
+        if (r.hours > 24) {
+          axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+        } else {
+          axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
+        }
+
+        if (!is.null(trueFaultTime)) abline(v=trueFaultTime, col="blue")
+
+      } else {
+        if (i %in% do) {
+          plot(x = dates,
+               y = data[,i],
+               xaxt="n",
+               ylim=c(0,3),
+               xlab = "",
+               ylab = "",
+               main = colnames(data)[i],
+               type = "p",
+               col = "black")
+          # line.ewma <- ewma(data[,i], n = 1440, alpha = 0.01)
+          # points(x = index(line.ewma),
+          #        y = line.ewma, col = "red")
+
+          if (r.hours > 24) {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+          } else {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
+          }
+
+          if (!is.null(trueFaultTime)) abline(v=trueFaultTime, col="blue")
+
+        } else {
+          plot(x = dates,
+               y = data[,i],
+               xaxt="n",
+               xlab = "",
+               ylab = "",
+               main = colnames(data)[i],
+               type = "p",
+               col = "black")
+          # line.ewma <- ewma(data[,i])
+          # line(x = index(line.ewma),
+          #      y = line.ewma)
+          if (r.hours > 24) {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+          } else {
+            axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
+          }
+          # axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
+          if (!is.null(trueFaultTime)) abline(v=trueFaultTime, col="blue")
+        }
+      }
+    }
+
+  }
   # png(file = paste(filename, ".png", sep=""), units = "in", res = 96, width = 11, height =n*3)
   # pdf(file = paste(filename,".pdf", sep=''),
   #     width = 30,
   #     height = (n)*2)
   svg(filename = paste(filename, ".svg", sep=""), width = 8, height =n*2, family = "serif")
-
-  par(mfrow=c((n),1), mar = c(2.1, 4.1, 4.1, 2.1))
-
-  for (i in 1:n) {
-
-    if (i %in% tmp) {
-      plot(x = dates,
-           y = data[,i],
-           xaxt="n",
-           ylim=c(0,7),
-           xlab = "",
-           ylab = "",
-           main = colnames(data)[i],
-           type = "p",
-           col = "black")
-      if (r.hours > 24) {
-        axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-      } else {
-        axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
-      }
-
-      if (!is.null(trueFaultTime)) abline(v=trueFaultTime, col="blue")
-
-      } else {
-      if (i %in% do) {
-        plot(x = dates,
-             y = data[,i],
-             xaxt="n",
-             ylim=c(0,3),
-             xlab = "",
-             ylab = "",
-             main = colnames(data)[i],
-             type = "p",
-             col = "black")
-        # line.ewma <- ewma(data[,i], n = 1440, alpha = 0.01)
-        # points(x = index(line.ewma),
-        #        y = line.ewma, col = "red")
-
-        if (r.hours > 24) {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-        } else {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
-        }
-
-        if (!is.null(trueFaultTime)) abline(v=trueFaultTime, col="blue")
-
-      } else {
-        plot(x = dates,
-             y = data[,i],
-             xaxt="n",
-             xlab = "",
-             ylab = "",
-             main = colnames(data)[i],
-             type = "p",
-             col = "black")
-        # line.ewma <- ewma(data[,i])
-        # line(x = index(line.ewma),
-        #      y = line.ewma)
-        if (r.hours > 24) {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-        } else {
-          axis.POSIXct(1, at = seq(r[1], r[2], by = "hours"), cex.axis = 1, format = "%H:%M")
-        }
-        # axis.POSIXct(1, at = seq(r[1], r[2], by = "days"), cex.axis = 1, format = "%m/%d")
-        if (!is.null(trueFaultTime)) abline(v=trueFaultTime, col="blue")
-      }
-    }
-  }
-
+  plot.it()
   dev.off()
-
+  # plot.it() # Margins too large to plot
 }
